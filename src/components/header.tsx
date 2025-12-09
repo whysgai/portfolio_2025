73b62b850b8011e0fe742d8daf42577193a0283e
Will Cohen <whysgai@gmail.com>
2025-12-09 10:49:02 -0500

    Infra: Initial commit; header and portfolio component creation

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+        <div className="header container">
+            <a className="header-name link tooling-text" href="index.html">Will&nbsp;<span className="header-middle-r">R</span>&nbsp;Cohen</a>
+            <div className="header-buttons">
+                <a className="header-nav-button link tooling-text" href="#Projects">Projects</a>
+                <a className="header-nav-button link tooling-text" href="html/experience.html">Experience</a>
+                <a className="header-nav-button link tooling-text" href="html/about.html">About</a>
+            </div>     
+        </div>
+    </header>
+  )
+}
