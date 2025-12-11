8fa92414d9bab9377bc02612b8cd9c2589e19651
Will Cohen <whysgai@gmail.com>
2025-12-11 10:37:26 -0500

    Refactor: move portfolio components under sub-directory

---
+++ src/components/Portfolio/ProjectButton.tsx
@@ -0,0 +1,41 @@
+import { ProjectTileButtonTypes } from "../../utilities/Enums";
+import { ProjectButtonProps } from "../../utilities/Interfaces";
+import { Strings } from "../../utilities/Strings";
+
+export default function ProjectButton ({type, url}: ProjectButtonProps) {
+
+    const text = (
+        type === ProjectTileButtonTypes.Case ? 
+            Strings.portfolio.project_tile.case
+            :
+            type === ProjectTileButtonTypes.Repo ?
+                Strings.portfolio.project_tile.repo
+                :
+                type === ProjectTileButtonTypes.Demo ?
+                    Strings.portfolio.project_tile.demo
+                    :
+                    "Error: Invalid button type"
+    );
+
+    const icon = (
+        type === ProjectTileButtonTypes.Case ? 
+            "bi-pen-fill"
+            :
+            type === ProjectTileButtonTypes.Repo ?
+                "bi-github"
+                :
+                type === ProjectTileButtonTypes.Demo ?
+                    "bi-play-circle-fill"
+                    :
+                    "Error: Invalid button type"
+    );
+
+    return (
+        <div className={`portfolio-button`}>
+            <a href={url} className={`link tooling-text`}>{text}
+                <i className={`portfolio-button-icon bi ${icon}`}></i>
+            </a>
+        </div>
+    )
+
+}
