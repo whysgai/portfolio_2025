dd4703ecb1a62bad3d018493475c8770305527e5
Will Cohen <whysgai@gmail.com>
2025-12-11 10:56:48 -0500

    Fix: Fix malfunctioning view change

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,37 @@ import React, { useState } from 'react';
 import logo from './assets/logo.svg';
 import Header from './components/Header'
 import Portfolio from './components/Portfolio/Portfolio'
+import Experience from './components/Experience/Experience';
+import About from './components/About/About';
 import './styles/App.css';
 import { View } from './utilities/Enums';
 
-const [view, setView] = useState(View.Portfolio); 
 
-function changeView(newView: View) {
-	setView(newView);
-};
 
 function App() {
-	return (
-		<div className="App">
-		<Header changeView={changeView} />
-		{
-			view === View.Portfolio ?
-				<Portfolio/>
-				:
-				view === View.Experience ?
-					<Portfolio/>
-					:
-					view === View.About ?
-						<Portfolio/>
-						:
-						<p>Error: Unknown view type</p>
-		}
-		
-		</div>
-	);
+    const [view, setView] = useState(View.Portfolio); 
+
+    function changeView(newView: View) {
+        console.log("Change view to: ", newView);
+        setView(newView);
+    };
+    return (
+        <div className="App">
+            <Header changeView={changeView} />
+            {
+                view === View.Portfolio ?
+                    <Portfolio/>
+                    :
+                    view === View.Experience ?
+                        <Experience/>
+                        :
+                        view === View.About ?
+                            <About/>
+                            :
+                            <p>Error: Unknown view type</p>
+            }        
+        </div>
+    );
 }
 
 export default App;
